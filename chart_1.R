563df685630d7cbe38fddef61639cc9e0d9392c3
sdang2 <sdang2@uw.edu>
2023-02-25 21:07:41 -0800

    finish chart1

--- chart_1.R
+++ chart_1.R
@@ -1,3 +1,5 @@
+# Physical or Virtual Book Checkouts over the past 4 years (before pandemic, during, and after)
+
 library(dplyr)
 library(stringr)
 library(ggplot2)
@@ -6,12 +8,11 @@ library(scales)
 # Read in data
 spl_10_checkout <- read.csv("~/Desktop/INFO201/2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
-# Filter data to last 4 years and select BOOK and EBOOK checkout records
+# Filter data to last 4 years of Book or EBook Records
 spl_10_checkout <- spl_10_checkout %>%
   filter(CheckoutYear >= as.numeric(format(Sys.Date(), "%Y")) - 4) %>%
   filter(str_detect(MaterialType, "BOOK|EBOOK")) %>%
   mutate(date = as.Date(paste0(CheckoutYear, "-", CheckoutMonth, "-01"), format = "%Y-%m-%d"))
-
 
 physicalbook_checkout <- spl_10_checkout %>%
   filter(str_detect(MaterialType, "BOOK")) %>%
@@ -26,7 +27,7 @@ virtualbook_checkout <- spl_10_checkout %>%
 # Merge data
 book_checkouts <- merge(physicalbook_checkout, virtualbook_checkout, by = "date", all = TRUE)
 
-# Create chart using ggplot2
+# Create time chart
 ggplot(book_checkouts, aes(x = date)) +
   geom_line(aes(y = physical_checkouts, color = "Physical Books"), size = 1) +
   geom_line(aes(y = virtual_checkouts, color = "Virtual Books"), size = 1) +
